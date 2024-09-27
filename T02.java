f8bbf3fc6b22745d56beab1c7c1a6db481ff8c09
rony <ronypangaribuan2505@gmail.com>
2024-09-27 10:40:45 +0700

    Telah Menyelesaikan Tugas

--- T02.java
+++ T02.java
@@ -1,1 +1,44 @@
-// NIM - Your Name
+// 12S24040 - Rony Reynaldy Pangaribuan
+// 12S24023 - Jaya Bestina Simbolon
+
+import java.util.*;
+import java.lang.Math;
+
+public class Program {
+    private static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String iSBN, judul, penulis, penerbit, formatBukuElektronik, kategori;
+        int tahunTerbit, stok;
+        double hargaPembelian, minimumMargin, rating;
+
+        iSBN = input.nextLine();
+        judul = input.nextLine();
+        penulis = input.nextLine();
+        tahunTerbit = input.nextInt();
+        penerbit = input.nextLine();
+        formatBukuElektronik = input.nextLine();
+        hargaPembelian = input.nextDouble();
+        minimumMargin = input.nextDouble();
+        stok = input.nextInt();
+        rating = input.nextDouble();
+        if (rating >= 4.7) {
+            kategori = "BestPick";
+        } else {
+            if (rating >= 4.5) {
+                kategori = "MustRead";
+            } else {
+                if (rating >= 4.0) {
+                    kategori = "Recommended";
+                } else {
+                    if (rating >= 3.0) {
+                        kategori = "Average";
+                    } else {
+                        kategori = "Low";
+                    }
+                }
+            }
+        }
+        System.out.println(iSBN + "|" + judul + "|" + penulis + "|" + tahunTerbit + "|" + penerbit + "|" + formatBukuElektronik + "|" + hargaPembelian + "|" + minimumMargin + "|" + stok + "|" + rating + "|" + kategori);
+    }
+}
